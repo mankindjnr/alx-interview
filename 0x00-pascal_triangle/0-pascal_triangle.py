3bb80d5952350d22eb51ee8bfafa06d3079c7508
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-06-30 15:47:20 +0400

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+def pascal_triangle(n):
+    major = []
+    for i in range(n):
+        a_list = []
+        break_flag = False
+
+        major_len = len(major)
+        for j in range(n - (n-(i+1))):
+            if major_len < 2:
+                if i > 0:   
+                    a_list.append(i)
+                else:
+                    a_list.append(i+1)
+            else:
+                a_list.append(1)
+                list_idx = major_len - 1
+                list_len = len(major[list_idx])
+                the_list = major[list_idx]
+
+                for tri in range(list_len - 1):
+                    a_list.append(the_list[tri] + the_list[tri + 1])
+
+                a_list.append(1)
+                break
+           
+        major.append(a_list)
+    
+    return major
+
+
