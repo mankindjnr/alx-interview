28d85b2797723ee25bf5fe2b95f365dd579f3aa1
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-06-30 15:53:28 +0400

    returning an empty list

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,31 +4,33 @@ this function returns a list of lists of integers representing the pascal's tria
 """
 def pascal_triangle(n):
     major = []
-    for i in range(n):
-        a_list = []
-        break_flag = False
+    if n <= 0:
+        return major
+    else:
+        for i in range(n):
+            a_list = []
+            break_flag = False
 
-        major_len = len(major)
-        for j in range(n - (n-(i+1))):
-            if major_len < 2:
-                if i > 0:   
-                    a_list.append(i)
+            major_len = len(major)
+            for j in range(n - (n-(i+1))):
+                if major_len < 2:
+                    if i > 0:   
+                        a_list.append(i)
+                    else:
+                        a_list.append(i+1)
                 else:
-                    a_list.append(i+1)
-            else:
-                a_list.append(1)
-                list_idx = major_len - 1
-                list_len = len(major[list_idx])
-                the_list = major[list_idx]
+                    a_list.append(1)
+                    list_idx = major_len - 1
+                    list_len = len(major[list_idx])
+                    the_list = major[list_idx]
 
-                for tri in range(list_len - 1):
-                    a_list.append(the_list[tri] + the_list[tri + 1])
+                    for tri in range(list_len - 1):
+                        a_list.append(the_list[tri] + the_list[tri + 1])
 
-                a_list.append(1)
-                break
-           
-        major.append(a_list)
-    
-    return major
-
+                    a_list.append(1)
+                    break
+            
+            major.append(a_list)
+        
+        return major
 
