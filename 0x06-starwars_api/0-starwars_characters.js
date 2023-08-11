c3115c41fbef33c2cbc2fdd45596e61f53155f49
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-11 06:31:55 -0700

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+// star wars api
 
 const request = require('request');
+const { promisify } = require('util');
+const asyncRequestGet = promisify(request.get);
 
 const film = process.argv[2];
 
@@ -16,21 +19,24 @@ request.get(apiUrl, (error, response, body) => {
 
       // Accessing the character value
       const characters = parsedBody.characters;
-      const numChar = characters.length;
 
       // retrieving the name of characters in film
-      for (let i = 0; i < numChar; i++) {
-        const charApi = characters[i];
-        request.get(charApi, (error, response, body) => {
-          if (error) {
-            console.error('Error:', error);
-          } else {
-            const parsedBody = JSON.parse(body);
+      async function fetchCharacters () {
+        for (const item of characters) {
+          try {
+            const charApi = item;
+            const response = await asyncRequestGet(charApi);
+
+            const parsedBody = JSON.parse(response.body);
             const name = parsedBody.name;
             console.log(name);
+          } catch (error) {
+            console.error('Error:', error);
           }
-        });
+        }
       }
+
+      fetchCharacters();
     } catch (parseError) {
       console.error('JSON Parse Error:', parseError);
     }
